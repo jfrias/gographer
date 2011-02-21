853019a80f574cb024f9da78ff788a465218b765
mwrulesc <mwrulesc@5e13f953-a89e-43fe-3da1-915b7fb197ce>
2011-02-21 22:58:54 +0000

    VCHEN Wrote the constructor, getGenesByNode and getNodesByGene functions for GOGeneGraph

--- trunk/test.py
+++ trunk/test.py
@@ -1,7 +1,11 @@
 from GOGraph import *
+from GOGeneGraph import *
 
 location = "./go_daily-termdb.obo-xml"
-#location = "./testgraph.obo-xml"
+location = "./testgraph.obo-xml"
 
 #possible namespaces are: "biological_process", "molecular_function", "cellular_component"
 GOGraphtest = GOGraph("biological_process", location)
+
+assoc = "./gene_association.goa_human"
+GOGenetest = GOGeneGraph(GOGraphtest,assoc)
