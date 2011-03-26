45d7c7624f0bc190f92ef9350230491dc22fda12
mwrulesc <mwrulesc@5e13f953-a89e-43fe-3da1-915b7fb197ce>
2011-03-26 06:47:59 +0000

    VCHEN Added StopwordList to utils.py. Changed calculateWordVector in GONode.py to remove given stop words. Added placeholder function calculateWordVectors in GOPubmedGraph.py

--- utils.py
+++ utils.py
@@ -440,3 +440,90 @@ class PorterStemmer:
         self.step4()
         self.step5()
         return self.b[self.k0:self.k+1]
+
+## A class to handle reading a list of stopwords and providing quick lookup
+class StopwordList:
+    ## Constructor
+    # @param fname The filename of the stopwords to read.  File should
+    # contain one word per line and already be lowercase
+    def __init__(self, fname=None):
+        self.words = set([])
+        if fname:
+            self.readFile(fname)
+        
+
+    ## Read a file that contains stopwords
+    # @param fname The filename of the stopwords to read.  File should
+    # contain one word per line and already be lowercase            
+    def readFile(self, fname):
+        words = []
+        try:
+            f = open(fname, 'r')
+            line = f.readline()
+            while line:
+                word = line.strip()
+                if word != "":
+                    words.append(word)
+                line = f.readline()
+            f.close()
+        except Exception, e:
+            msg = "Could not read file \"%s\": %s\nWill NOT be using any stopwords"
+            print msg % (fname, str(e))
+        self.addWords(words)
+
+
+    ## Handle queries like "word in stopwords"
+    # \return True only if word is in this list
+    def __contains__(self, word):
+        return word in self.words
+
+
+    ## Same as __contains__
+    # \return True only if word is in this list
+    def hasWord(self, word):
+        return word in self
+
+
+    ## Add a list of words to the stopwords
+    # @param words A set or list of words
+    def addWords(self, words):
+        self.words.update(words)
+
+
+    ## Get the Python list that this class operates on
+    # \return a Python list of the words contained in this list
+    def getList(self):
+        return [word for word in self.words]
+
+
+    ## Return a new StopwordList composed of the words in this one and
+    # another.
+    # \return A new StopwordList
+    def __add__(self, other):
+        sl = StopwordList()
+        sl.words.update(self.words, other.words)
+        return sl
+
+
+    ## Determine if another stopwordlist is the same as this one
+    # \return True if same, False otherwise
+    def __eq__(self, other):
+        return self.words == other.words
+
+
+    ## Determine if another stopwordlist is the same as this one
+    # \return False if same, True otherwise
+    def __neq__(self, other):
+        return not self == other
+
+
+    ## Add a list of stopwordlists together to form one superlist
+    # @param stopwordLists A list of StopwordList objects
+    # \return A StopwordList that is the set that contains the union of the ones given
+    @classmethod
+    def join(klass, stopwordLists):
+        sl = StopwordList()
+        words = [swl.words for swl in stopwordLists]
+        sl.words.update(*words)
+        return sl
+
