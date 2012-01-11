bd7beb8473f10fa2e7ce144f911bb1f19214bec2
MwrulesC <MwrulesC@5e13f953-a89e-43fe-3da1-915b7fb197ce>
2012-01-11 19:41:17 +0000

    VCHEN Made some changes to test.py

--- test.py
+++ test.py
@@ -6,8 +6,6 @@ from utils import *
 from Corpus import *
 from SteinerTree import *
 
-
-#location = "./go_daily-termdb.obo-xml"
 location = "./testgraph.obo-xml"
 
 #possible namespaces are: "biological_process", "molecular_function", "cellular_component"
@@ -20,15 +18,15 @@ if GOGraphtest.getNodeDescription('GO:0046431') != descrip:
 if GOGraphtest.getNodeNamespace('GO:0046431') != namespace:
     print "Error in getNodeNamespace"
 
-assoc = "./gene_association.goa_human"
+assoc = "./gene_association.test"
 GOGenetest = GOGeneGraph(GOGraphtest,assoc)
 
-genes = set([('P05091', ''), ('Q8NE62', ''), ('P47895', ''), ('P48448', ''), ('P00326', ''), ('P43353', ''), ('P08319', ''), ('P07327', '')])
-propGenes = set([('Q8NE62', ''), ('P47895', ''), ('O43704', ''), ('P48448', ''), ('P43353', ''), ('P05091', ''), ('P00326', ''), ('P08319', ''), ('P07327', '')])
-nodesByGene = set(['GO:0006066', 'GO:0008152'])
+genes = set([('Q8NE62', ''), ('P48448', ''), ('P43353', ''), ('P05091', ''), ('P07327', ''), ('P08319', '')])
+propGenes = set([('P05091', ''), ('Q8NE62', ''), ('O43704', ''), ('P48448', ''), ('P07327', ''), ('P43353', ''), ('P08319', '')])
+nodesByGene = set(['GO:0006725', 'GO:0032787'])
 if GOGenetest.getGenesByNode('GO:0006066') != genes:
     print "Error in getGenesByNode"
-if GOGenetest.getNodesByGene('P48448') != nodesByGene:
+if GOGenetest.getNodesByGene('P05177') != nodesByGene:
     print "Error in getNodesByGene"
 if GOGenetest.getPropagatedGenesByNode('GO:0006066') != propGenes:
     print "Error in getPropagatedGenesByNode"
@@ -36,15 +34,15 @@ if GOGenetest.getPropagatedGenesByNode('GO:0006066') != propGenes:
 
 GOPubmedtest = GOPubmedGraph(GOGraphtest,assoc)
 
-pubmed = set([('2347582', ''), ('7698756', ''), ('1306115', ''), ('3466164', ''), ('7828891', ''), ('8890755', '')])
-propPubmed = set([('10203017', ''), ('7959792', ''), ('9473496', ''), ('7663508', ''), ('10692104', ''), ('6548414', ''), ('3466164', ''), ('10359671', ''), ('11300766', ''), ('11254442', ''), ('9000139', ''), ('10022914', ''), ('8822211', ''), ('10373482', ''), ('1695324', ''), ('1374385', ''), ('3800996', ''), ('12882974', ''), ('3755672', ''), ('9500206', ''), ('8890755', ''), ('8353497', ''), ('9733774', ''), ('9263035', ''), ('8707889', ''), ('9295054', ''), ('7698756', ''), ('8333863', ''), ('7912130', ''), ('9207799', ''), ('11597136', ''), ('9305759', ''), ('2347582', ''), ('10101297', ''), ('8359595', ''), ('11606197', ''), ('1918003', ''), ('1306115', ''), ('9268630', ''), ('9299468', ''), ('10382971', ''), ('9463486', ''), ('11179693', ''), ('9237672', ''), ('7828891', ''), ('9207021', ''), ('2110361', '')])
+pubmed = set([('1306115', ''), ('8890755', ''), ('2347582', ''), ('3466164', ''), ('7828891', '')])
+propPubmed = set([('2347582', ''), ('1306115', ''), ('9463486', ''), ('8890755', ''), ('7828891', ''), ('3466164', '')])
 if GOPubmedtest.getPubMedByNode('GO:0006066') != pubmed:
     print "Error in getPubMedByNode"
 for i in pubmed:
     if GOPubmedtest.getNodesByPubMed(i[0]) != set(['GO:0006066']):
         print "Error in getNodesByPubmed"
         break
-if GOPubmedtest.getPropagatedPubMedByNode('GO:0008150') != propPubmed:
+if GOPubmedtest.getPropagatedPubMedByNode('GO:0006066') != propPubmed:
     print "Error in getPropagatedPubMedByNode"
 
 corpus = Corpus.fromPubmedArticleSetFile("test_block_0.xml")
