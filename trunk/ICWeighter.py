f74adb150586484be9cd5f42d71379b3c57c9851
MwrulesC <MwrulesC@5e13f953-a89e-43fe-3da1-915b7fb197ce>
2012-05-25 06:33:37 +0000

    VCHEN Created ICWeighter.py

---
+++ trunk/ICWeighter.py
@@ -0,0 +1,41 @@
+from WeightingInterface import WeightingInterface
+from networkx import topological_sort
+import math
+
+class ICWeighter(WeightingInterface):
+    def __init__(self, graph=None, smoother=0.001):
+        self.graphID = None
+        self.totalAnnotation = 0
+        self.smoother = smoother
+        if graph:
+            self.graphID = id(graph)
+            for node in graph:
+                totalAnnotation += len(graph.getPropagatedPubMedByNode(node).union(graph.getPubMedByNode(node)))
+            self.totalAnnotation = totalAnnotation
+    
+    def makeWeighted(self, child, parent, graph):
+        if id(graph) != self.graphID:
+            self.storeTotalAnnotation(graph)
+        return self.calcWeightIC(child, parent, graph)
+            
+    def storeTotalAnnotation(self, graph):
+        self.graphID = id(graph)
+        for node in graph:
+            totalAnnotation += len(graph.getPropagatedPubMedByNode(node).union(graph.getPubMedByNode(node)))
+        self.totalAnnotation = totalAnnotation
+
+    def setSmoother(self, smoother):
+        self.smoother = smoother
+
+    def calcWeightIC(self, child, parent, graph):
+        childAnnotation = len(graph.getPropagatedPubMedByNode(child).union(graph.getPubMedByNode(child)))
+        parentAnnotation = len(graph.getPropagatedPubMedByNode(parent).union(graph.getPubMedByNode(parent)))
+        if childAnnotation == 0:
+            childAnnotation = self.smoother
+        if parentAnnotation == 0:
+            parentAnnotation = self.smoother
+        childIC = -math.log(float(childAnnotation)/self.totalAnnotation)
+        parentIC = -math.log(float(parentAnnotation)/self.totalAnnotation)
+        return abs(parentIC-childIC)
+        
+        
